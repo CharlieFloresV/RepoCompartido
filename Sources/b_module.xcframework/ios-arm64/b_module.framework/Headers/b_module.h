4d42c484faf7c9c216ae3563fcd9d424502b3f2b
Carlos Andres Flores de la Vega <carlosflores@ext.gendra.dev>
2024-03-27 11:09:33 -0600

    upd8

---
+++ Sources/b_module.xcframework/ios-arm64/b_module.framework/Headers/b_module.h
@@ -0,0 +1,18 @@
+//
+//  b_module.h
+//  b_module
+//
+//  Created by CARLOS ANDRES FLORES DE LA VEGA on 25/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for b_module.
+FOUNDATION_EXPORT double b_moduleVersionNumber;
+
+//! Project version string for b_module.
+FOUNDATION_EXPORT const unsigned char b_moduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <b_module/PublicHeader.h>
+
+
